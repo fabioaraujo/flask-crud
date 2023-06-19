ae696f0bddc3d02af1bb3b33da75f47830feba65
Fabio Araujo <fabioaraujods@gmail.com>
2023-06-18 21:10:42 -0300

    organizando o código

---
+++ config.py
@@ -0,0 +1,14 @@
+from jogoteca import app
+import os
+from dotenv import load_dotenv
+
+SECRET_KEY = 'alura'
+
+SQLALCHEMY_DATABASE_URI = \
+    "{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(
+        SGBD = "mysql+mysqlconnector",
+        usuario = os.getenv("USER_DB"),
+        senha = os.getenv("PWD_DB"),
+        servidor = os.getenv("HOST"),
+        database = "jogoteca"
+    )
